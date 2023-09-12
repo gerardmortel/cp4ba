0314f4da45c9a44558455fb2be03c7a6d762a0d2
Gerard Mortel <gmortel@us.ibm.com>
2023-09-12 09:01:58 -0500

    Deleted unnecessary lines

--- 07_configure_cluster_by_script.sh
+++ 07_configure_cluster_by_script.sh
@@ -5,4 +5,9 @@ cp ibm-cp-automation/inventory/cp4aOperatorSdk/files/deploy/crs/cert-kubernetes/
 
 echo "#### Configure cluster by script"
 cd ibm-cp-automation/inventory/cp4aOperatorSdk/files/deploy/crs/cert-kubernetes/scripts
-./cp4a-clusteradmin-setup.sh
+#./cp4a-clusteradmin-setup.sh
+
+# 2 2 cp4ba1 2 ${API_KEY_GENERATED}
+printf "%s\n" 2 2 ${CP4BANAMESPACE} 2 ${API_KEY_GENERATED} | ./cp4a-clusteradmin-setup.sh
+
+printf "%s\n" 2 2 1 2 1 BAWDB,BAWAUDB,UMSDB,APPDB,AAEDB,AEOS,BAWTOS,BAWDOS,BAWDOCS,OS1DB,ICNDB,GCDDB nfs-managed-storage nfs-managed-storage nfs-managed-storage | ./baw-prerequisites.sh -m property
