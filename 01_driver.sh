9170031345e029da09a168c655ecaf4934c41b4a
Gerard Mortel <gmortel@us.ibm.com>
2023-09-14 12:50:02 -0500

    Updated list and order

--- 01_driver.sh
+++ 01_driver.sh
@@ -10,6 +10,7 @@ echo "#### Running the driver file"
 ./08_cp4a_prerequisites_property.sh
 ./09_edit_property_files.sh
 ./10_cp4a_prerequisites_generate.sh
-./11_edit_final_cr.sh
-./12_create_secrets.sh
-./13_apply_cr.sh
+./11_create_secrets.sh
+./12_generate_final_cr.sh
+./13_edit_final_cr.sh
+./14_apply_cr.sh
