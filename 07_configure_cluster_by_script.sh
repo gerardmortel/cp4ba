201b58083e0bffd2e0280270a65a6b2a7b86501e
Gerard Mortel <gmortel@us.ibm.com>
2024-02-27 20:46:16 -0600

    Updated for 23.0.2

--- 07_configure_cluster_by_script.sh
+++ 07_configure_cluster_by_script.sh
@@ -19,7 +19,10 @@ if [ ${IS_FIRST_CLOUDPAK_IN_CLUSTER} == "false"  ]; then
 ./cp4a-clusteradmin-setup.sh <<END
 2
 2
+No
+Yes
 ${CP4BANAMESPACE}
+2
 Yes
 2
 Yes
@@ -27,13 +30,73 @@ ${API_KEY_GENERATED}
 END
 else
   echo "#### This is the first cloudpak in the cluster.  The response does not need an extra Yes to confirm installation"
-./cp4a-clusteradmin-setup.sh <<END
+  ./cp4a-clusteradmin-setup.sh <<END
 2
 2
+No
 Yes
 ${CP4BANAMESPACE}
 2
 Yes
 ${API_KEY_GENERATED}
 END
+
+# [INFO] Setting up the cluster for IBM Cloud Pak for Business Automation
+
+# Select the cloud platform to deploy: 
+# 1) RedHat OpenShift Kubernetes Service (ROKS) - Public Cloud
+# 2) Openshift Container Platform (OCP) - Private Cloud
+# 3) Other ( Certified Kubernetes Cloud Platform / CNCF)
+# Enter a valid option [1 to 3]: 2
+
+# What type of deployment is being performed?
+# ATTENTION: The BAI standalone only supports "Production" deployment type.
+# 1) Starter
+# 2) Production
+# Enter a valid option [1 to 2]: 2
+
+# [NOTES] If you are planning to enable FIPS for CP4BA deployment, this script can perform a check on the OCP cluster to ensure the compute nodes have FIPS enabled.
+# Do you want to proceed with this check? (Yes/No, default: No): No
+
+# [NOTES] You can install the CP4BA deployment as either a private catalog (namespace scope) or the global catalog namespace (GCN). The private option uses the same target namespace of the CP4BA deployment, the GCN uses the openshift-marketplace namespace.
+# Do you want to deploy CP4BA using private catalog? (Yes/No, default: No): Yes
+
+# Where do you want to deploy Cloud Pak for Business Automation?
+# Enter the name for a new project or an existing project (namespace): cp4ba
+
+# This script prepares the OLM for the deployment of some Cloud Pak for Business Automation capabilities 
+
+# Here are the existing users on this cluster: 
+# 1) Cluster Admin
+# 2) gerard
+# Enter an existing username in your cluster, valid option [1 to 2], non-admin is suggested: 2
+
+# [INFO] Creating cp4ba-fips-status configMap in the project "cp4ba"
+
+# [✔] Created cp4ba-fips-status configMap in the project "cp4ba".
+
+# Follow the instructions on how to get your Entitlement Key: 
+# https://www.ibm.com/docs/en/cloud-paks/cp-biz-automation/23.0.2?topic=deployment-getting-access-images-from-public-entitled-registry
+
+# Do you have a Cloud Pak for Business Automation Entitlement Registry key (Yes/No, default: No): Yes
+
+# Enter your Entitlement Registry key: 
+# Verifying the Entitlement Registry key...
+# Login Succeeded!
+# Entitlement Registry key is valid.
+
+# The existing storage classes in the cluster: 
+# NAME                            PROVISIONER        RECLAIMPOLICY   VOLUMEBINDINGMODE   ALLOWVOLUMEEXPANSION   AGE
+# nfs-managed-storage (default)   cp4ba/nfs-client   Delete          Immediate           false                  102m
+# Creating docker-registry secret for Entitlement Registry key in project cp4ba...
+# secret/ibm-entitlement-key created
+# Done
+
+# [INFO] Applying the latest IBM CP4BA Operator catalog source...
+
+# [✔] IBM CP4BA Operator catalog source Updated!
+
+# [INFO] Starting to install IBM Cert Manager and IBM Licensing Operator ...
+
+
 fi
--- 08_cp4a_prerequisites_property.sh
+++ 08_cp4a_prerequisites_property.sh
@@ -15,5 +15,68 @@ ${STORAGECLASS}
 ${STORAGECLASS}
 ${BLOCKSTORAGECLASS}
 1
+1
 dbserver1
+${CP4BANAMESPACE}
+Yes
 END
+
+# 2 Operational Decision Manager
+#   Nothing else to add
+# 1 Decision Center
+# 2 Rule Execution Server
+# 3 Decision Runner
+#   Nothing else to add
+# 2 IBM Secure Directory Server
+# ${STORAGECLASS} storage classname for slow storage(RWX)
+# ${STORAGECLASS} storage classname for medium storage(RWX)
+# ${STORAGECLASS} storage classname for fast storage(RWX)
+# ${BLOCKSTORAGECLASS} storage classname for Zen(RWO)
+# 1 deployment profile (default: small) 1=small, 2=medium, 3=large
+# 1 Database type 1=IBM Db2 Database, 2=Oracle, 3=Microsoft SQL Server, 4=PostgreSQL
+# dbserver1 Enter the alias name(s) for the database server(s)/instance(s) to be used by the CP4BA deployment
+# ${CP4BANAMESPACE} Enter the name for an existing project (namespace)
+# Yes Do you want to restrict network egress to unknown external destination for this CP4BA deployment? (Notes: CP4BA 23.0.2 prevents all network egress to unknown destinations by default. You can either (1) enable all egress or (2) accept the new default and create network policies to allow your specific communication targets as documented in the knowledge center.) (Yes/No, default: Yes)
+
+# ============== Creating database and LDAP property files for CP4BA ==============
+# Creating DB Server property file for CP4BA
+# [✔] Created the DB Server property file for CP4BA
+
+# Creating LDAP Server property file for CP4BA
+# [✔] Created the LDAP Server property file for CP4BA
+
+
+# ============== Creating property file for database name and user required by CP4BA ==============
+# Creating Property file for IBM Business Automation Navigator
+# [✔] Created Property file for IBM Business Automation Navigator
+
+# Creating Property file for IBM Operational Decision Manager
+# [✔] Created Property file for IBM Operational Decision Manager
+
+
+# ============== Created all property files for CP4BA ==============
+# [NEXT ACTIONS]
+# Enter the <Required> values in the property files under /root/cp4ba-23.0.2/ibm-cp-automation/inventory/cp4aOperatorSdk/files/deploy/crs/cert-kubernetes/scripts/cp4ba-prerequisites/propertyfile
+# [*] The key name in the property file is created by the cp4a-prerequisites.sh and is NOT EDITABLE.
+# [*] The value in the property file must be within double quotes.
+# [*] The value for User/Password in [cp4ba_db_name_user.property] [cp4ba_user_profile.property] file should NOT include special characters: single quotation "'"
+# [*] The value in [cp4ba_LDAP.property] or [cp4ba_External_LDAP.property] [cp4ba_user_profile.property] file should NOT include special character '"'
+# * [cp4ba_db_server.property]:
+#   - Properties for database server used by CP4BA deployment, such as DATABASE_SERVERNAME/DATABASE_PORT/DATABASE_SSL_ENABLE.
+
+#   - The value of "<DB_SERVER_LIST>" is an alias for the database servers. The key supports comma-separated lists.
+
+# * [cp4ba_db_name_user.property]:
+#   - Properties for database name and user name required by each component of the CP4BA deployment, such as GCD_DB_NAME/GCD_DB_USER_NAME/GCD_DB_USER_PASSWORD.
+
+#   - Change the prefix "<DB_SERVER_NAME>" to assign which database is used by the component.
+
+#   - The value of "<DB_SERVER_NAME>" must match the value of <DB_SERVER_LIST> that is defined in "<DB_SERVER_LIST>" of "cp4ba_db_server.property".
+
+# * [cp4ba_LDAP.property]:
+#   - Properties for the LDAP server that is used by the CP4BA deployment, such as LDAP_SERVER/LDAP_PORT/LDAP_BASE_DN/LDAP_BIND_DN/LDAP_BIND_DN_PASSWORD.
+
+# * [cp4ba_user_profile.property]:
+#   - Properties for the global value used by the CP4BA deployment, such as "sc_deployment_license".
+
+#   - properties for the value used by each component of CP4BA, such as <APPLOGIN_USER>/<APPLOGIN_PASSWORD>
