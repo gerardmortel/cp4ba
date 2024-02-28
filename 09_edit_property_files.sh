8b6b5132a89e52ee96a9bb3dca26c77d47077094
Gerard Mortel <gmortel@us.ibm.com>
2024-02-28 08:43:53 -0600

    Updated to 23.0.2

--- 09_edit_property_files.sh
+++ 09_edit_property_files.sh
@@ -6,7 +6,7 @@ cd ibm-cp-automation/inventory/cp4aOperatorSdk/files/deploy/crs/cert-kubernetes/
 echo "#### Edit cp4ba_db_name_user.property"
 cp -p cp4ba_db_name_user.property cp4ba_db_name_user.property.orig
 sed -r "s/USER_NAME=\"<youruser1>\"/USER_NAME=\"${DB_USERNAME}\"/g" cp4ba_db_name_user.property > cp4ba_db_name_user.property.1
-sed -r "s/USER_PASSWORD=\"<yourpassword>\"/USER_PASSWORD=\"${DB_PASSWORD}\"/g" cp4ba_db_name_user.property.1 > cp4ba_db_name_user.property.2
+sed -r "s/USER_PASSWORD=\"\{Base64\}<yourpassword>\"/USER_PASSWORD=\"${DB_PASSWORD}\"/g" cp4ba_db_name_user.property.1 > cp4ba_db_name_user.property.2
 
 echo "#### Copy last file to first file"
 rm -f cp4ba_db_name_user.property
@@ -30,7 +30,7 @@ sed -r "s/LDAP_SERVER=\"<Required>\"/LDAP_SERVER=\"${LDAP_SERVER}\"/g" cp4ba_LDA
 sed -r "s/LDAP_PORT=\"<Required>\"/LDAP_PORT=\"${LDAP_PORT}\"/g" cp4ba_LDAP.property.1 > cp4ba_LDAP.property.2
 sed -r "s/LDAP_BASE_DN=\"<Required>\"/LDAP_BASE_DN=\"${LDAP_BASE_DN}\"/g" cp4ba_LDAP.property.2 > cp4ba_LDAP.property.3
 sed -r "s/LDAP_BIND_DN=\"<Required>\"/LDAP_BIND_DN=\"${LDAP_BIND_DN}\"/g" cp4ba_LDAP.property.3 > cp4ba_LDAP.property.4
-sed -r "s/LDAP_BIND_DN_PASSWORD=\"<Required>\"/LDAP_BIND_DN_PASSWORD=\"${LDAP_BIND_DN_PASSWORD}\"/g" cp4ba_LDAP.property.4 > cp4ba_LDAP.property.5
+sed -r "s/LDAP_BIND_DN_PASSWORD=\"\{Base64\}<Required>\"/LDAP_BIND_DN_PASSWORD=\"${LDAP_BIND_DN_PASSWORD}\"/g" cp4ba_LDAP.property.4 > cp4ba_LDAP.property.5
 sed -r "s/LDAP_SSL_ENABLED=\"True\"/LDAP_SSL_ENABLED=\"${LDAP_SSL_ENABLED}\"/g" cp4ba_LDAP.property.5 > cp4ba_LDAP.property.6
 sed -r 's|LDAP_USER_NAME_ATTRIBUTE="<Required>"|LDAP_USER_NAME_ATTRIBUTE="*:uid"|g' cp4ba_LDAP.property.6 > cp4ba_LDAP.property.7
 sed -r 's|LDAP_USER_DISPLAY_NAME_ATTR="<Required>"|LDAP_USER_DISPLAY_NAME_ATTR="cn"|g' cp4ba_LDAP.property.7 > cp4ba_LDAP.property.8
@@ -57,11 +57,11 @@ sed -r "s/(CP4BA\.CP4BA_LICENSE=)\"<Required>\"/\1\"non-production\"/g" cp4ba_us
 ## Provide the user name for BAN. For example: "BANAdmin"
 sed -r "s/BAN\.APPLOGIN_USER=\"<Required>\"/BAN\.APPLOGIN_USER=\"${P8ADMINUSER}\"/g" cp4ba_user_profile.property.1 > cp4ba_user_profile.property.2
 ## Provide the user password for BAN.
-sed -r "s/BAN\.APPLOGIN_PASSWORD=\"<Required>\"/BAN\.APPLOGIN_PASSWORD=\"${P8ADMINPASSWORD}\"/g" cp4ba_user_profile.property.2 > cp4ba_user_profile.property.3
+sed -r "s/BAN\.APPLOGIN_PASSWORD=\"\{Base64\}<Required>\"/BAN\.APPLOGIN_PASSWORD=\"${P8ADMINPASSWORD}\"/g" cp4ba_user_profile.property.2 > cp4ba_user_profile.property.3
 ## Provide LTPA key password for BAN deployment.
-sed -r "s/BAN\.LTPA_PASSWORD=\"<Required>\"/BAN\.LTPA_PASSWORD=\"${BANLTPAPASSWORD}\"/g" cp4ba_user_profile.property.3 > cp4ba_user_profile.property.4
+sed -r "s/BAN\.LTPA_PASSWORD=\"\{Base64\}<Required>\"/BAN\.LTPA_PASSWORD=\"${BANLTPAPASSWORD}\"/g" cp4ba_user_profile.property.3 > cp4ba_user_profile.property.4
 ## Provide keystore password for BAN deployment.
-sed -r "s/BAN\.KEYSTORE_PASSWORD=\"<Required>\"/BAN\.KEYSTORE_PASSWORD=\"${BANKEYSTOREPASSWORD}\"/g" cp4ba_user_profile.property.4 > cp4ba_user_profile.property.5
+sed -r "s/BAN\.KEYSTORE_PASSWORD=\"\{Base64\}<Required>\"/BAN\.KEYSTORE_PASSWORD=\"${BANKEYSTOREPASSWORD}\"/g" cp4ba_user_profile.property.4 > cp4ba_user_profile.property.5
 ## Provide the user name for jMail used by BAN. For example: "jMailAdmin"
 sed -r "s/BAN\.JMAIL_USER_NAME=\"<Optional>\"/BAN\.JMAIL_USER_NAME=\"${BANJMAILUSERNAME}\"/g" cp4ba_user_profile.property.5 > cp4ba_user_profile.property.6
 ## Provide the user password for jMail used by BAN.
